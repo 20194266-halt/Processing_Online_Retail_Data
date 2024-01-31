19ef5e604c21771ea9667e57b3e6bb5ad17d7705
20194266-halt <hadatascience01@gmail.com>
2024-01-31 08:49:22 +0700

    fix: crawl

--- airflow/dags/crawl.py
+++ airflow/dags/crawl.py
@@ -1,77 +1,3 @@
-# import requests
-# import json
-# import csv
-# from datetime import datetime
-# from hdfs import InsecureClient
-# from fake_useragent import UserAgent
-
-# class LazSpider:
-#     def __init__(self):
-#         self.name = 'laz'
-#         self.allowed_domains = ['www.lazada.vn']
-#         self.ua = UserAgent()
-#         self.page = 1
-#         self.hdfs_path = "http://localhost:9870"
-#         self.hdfs_user = 'root'
-#         self.client = InsecureClient("http://localhost:9870", user='root')
-#         self.category = 'Electronic Devices'
-
-#     def start_requests(self):
-#         url = f'https://www.lazada.vn/dien-thoai/?ajax=true&page={self.page}&spm=a2o42.searchlistcategory.cate_5.1.46281e22mYNSDT'
-#         headers = {
-#             'User-Agent': self.ua.random,
-#             'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
-#             'accept-encoding': 'gzip, deflate, br',
-#             'accept-language': 'en-US,en;q=0.9',
-#             'cookie': 'your_cookie_string_here',
-#         }
-
-#         response = requests.get(url, headers=headers)
-#         self.parse(response)
-
-#     def flush_to_hdfs(self, csv_converted_content):
-#         timestamp = datetime.now().strftime('%Y-%m-%d')
-#         hdfs_path = f'/data/lazada_{timestamp}.csv'
-#         with self.client.write(hdfs_path, encoding='utf-8', overwrite=True) as writer:
-#             writer.write(csv_converted_content)
-
-#     def parse(self, response):
-#         data = json.loads(response.text)
-#         try:
-#             existing_data = [['NAME', 'productUrl', 'imageUrl',
-#                               'originalPrice', 'DiscountedPrice', 'Discount',
-#                               'ratingScore', 'review', 'description',
-#                               'categories', 'itemId', 'page']]
-#             for item in data.get('mods').get('listItems'):
-#                 existing_data.append([item.get('name', '').replace(',', '').replace("'", '').replace('"', ''),
-#                                       item.get('productUrl'),
-#                                       item.get('image'),
-#                                       item.get('originalPrice'),
-#                                       item.get('price'),
-#                                       item.get('discount'),
-#                                       item.get('ratingScore'),
-#                                       item.get('review'),
-#                                       item.get('description', 'Electrical Device'),
-#                                       self.category,
-#                                       item.get('itemSoldCntShow'),
-#                                       item.get('sellerName'),
-#                                       item.get('brandName'),
-#                                       item.get('location')])
-
-#             csv_converted_content = '\n'.join([','.join(map(str, row)) for row in existing_data])
-#             # self.flush_to_hdfs(csv_converted_content)
-#             self.flush_to_hdfs(csv_converted_content)
-#             with open('output.csv', 'w') as file:
-#                 file.write(csv_converted_content)
-
-#         except TypeError:
-#             print("NO PAGE LEFT TO SCRAPE")
-#             sys.exit(0)
-#         except AttributeError:
-#             print("Blocked on page number:", self.page)
-# if __name__ == "__main__":
-#     laz_spider = LazSpider()
-#     laz_spider.start_requests()
 import requests
 import json
 import csv
@@ -80,6 +6,11 @@ from hdfs import InsecureClient
 from fake_useragent import UserAgent
 
 ua = UserAgent()
+hdfs_path = "http://localhost:9870"
+hdfs_user = 'root'
+global client
+client = InsecureClient("http://localhost:9870", user='root')
+
 url = f'https://www.lazada.sg/mother-baby/?ajax=true&page=9&spm=a2o42.searchlistcategory.cate_5.1.46281e22mYNSDT'
 headers = {
     'User-Agent': ua.random,
@@ -89,30 +20,40 @@ headers = {
     'cookie': 'your_cookie_string_here',
 }
 
-response = requests.get(url, headers=headers)
-data = response.json()
-#print(data['mods'])
-for key in data['mods']['listItems']:
-    print(key)
-# for item in data.get('mods').get('listItems'):
-#     existing_data = [['NAME', 'productUrl', 'imageUrl',
-#                               'originalPrice', 'DiscountedPrice', 'Discount',
-#                               'ratingScore', 'review', 'description',
-#                               'categories', 'itemId', 'page']]
-#     for item in data.get('mods').get('listItems'):
-#         existing_data.append([item.get('name', '').replace(',', '').replace("'", '').replace('"', ''),
-#                                       item.get('productUrl'),
-#                                       item.get('image'),
-#                                       item.get('originalPrice'),
-#                                       item.get('price'),
-#                                       item.get('discount'),
-#                                       item.get('ratingScore'),
-#                                       item.get('review'),
-#                                       item.get('description', 'Electrical Device'),
-#                                       self.category,
-#                                       item.get('itemSoldCntShow'),
-#                                       item.get('sellerName'),
-#                                       item.get('brandName'),
-#                                       item.get('location')])
-#         print(existing_data)
+def flush_to_hdfs(csv_converted_content):
+    timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    hdfs_path = f'/data/lazada_{timestamp}.csv'
+    with client.write(hdfs_path, encoding='utf-8', overwrite=True) as writer:
+        writer.write(csv_converted_content)
 
+response = requests.get(url, headers=headers)
+print("llllllllllllllllllllllllllllllllllllllllllllllllll",response)
+data = response.json()
+#print(data.get('mods').get('listItems'))
+# for key in data['mods']['listItems']:
+#    print(key)
+for item in data.get('mods').get('listItems'):
+    existing_data = [['NAME', 
+                              'originalPrice', 'DiscountedPrice', 'Discount',
+                              'ratingScore', 'review', 'description',
+                              'categories', 'itemSoldCntShow', 'sellerName', 'brandName','location' ]]
+    for item in data.get('mods').get('listItems'):
+        existing_data.append([item.get('name', '').replace(',', '').replace("'", '').replace('"', ''),
+                                      item.get('originalPrice'),
+                                      item.get('price'),
+                                      item.get('discount'),
+                                      item.get('ratingScore'),
+                                      item.get('review'),
+                                      "Mom and Baby",
+                                      item.get('itemSoldCntShow'),
+                                      item.get('sellerName'),
+                                      item.get('brandName'),
+                                      item.get('location')])
+    csv_converted_content = '\n'.join([','.join(map(str, row)) for row in existing_data])
+    flush_to_hdfs(csv_converted_content)
+    # timestamp = datetime.now().strftime('%Y-%m-%d')
+    # csv_file_path = f'lazada.csv'  
+
+    # with open(csv_file_path, 'w', encoding='utf-8') as file:
+    #     file.write(csv_converted_content)
+print("crawling....")
--- airflow/dags/spark.py
+++ airflow/dags/spark.py
@@ -16,25 +16,19 @@ hdfs_host = '172.21.0.5'
 hdfs_port = 9000
 hdfs_directory = '/data'
 timestamp = datetime.now().strftime('%Y-%m-%d')
-hdfs_path = '/data/output.csv'#f'data/lazada_{timestamp}.csv'
+hdfs_path = f'hdfs://namenode:9000/data/lazada_{timestamp}.csv'
 
 from pyspark.sql.types import *
 schema = StructType([
     StructField('NAME', StringType(), True),
-    StructField('productUrl', StringType(), True),
-    StructField('imageUrl', StringType(), True),
     StructField('originalPrice', DoubleType(), True),
     StructField('DiscountedPrice', DoubleType(),True),
     StructField('Discount', StringType(), True),
     StructField('ratingScore', DoubleType(),True),
     StructField('review', IntegerType(),True),
-    StructField('description', StringType(), True),
     StructField('categories', StringType(), True),
-    StructField('itemId', DoubleType(),True),
-    StructField('itemSoldCntShow', IntegerType(), True),
+    StructField('itemSoldCntShow', StringType(), True),
     StructField('sellerName', StringType(), True),
-    StructField('sellerId', StringType(), True),
-    StructField('brandId', StringType(), True),
     StructField('brandName', StringType(), True),
     StructField('location', StringType(), True)
 
@@ -44,11 +38,11 @@ df = (spark.read.format("com.databricks.spark.csv")
         .option("header", "true")
         #.option("inferSchema","true")
         .schema(schema)
-        .load('hdfs://namenode:9000/data/output.csv'))
+        .load(hdfs_path))
 
 df.show()
 #
-filtered_df = df.select(['NAME', 'originalPrice','DiscountedPrice','Discount', 'ratingScore', 'review', 'categories', 'itemSoldCntShow', 'sellerName', 'sellerId','brandId','brandName','location'])
+filtered_df = df.select(['NAME', 'originalPrice','DiscountedPrice','Discount', 'ratingScore', 'review', 'categories', 'itemSoldCntShow', 'sellerName','brandName','location'])
 
 average_value = filtered_df.agg(avg(col('DiscountedPrice'))).collect()[0][0]
 filtered_df = filtered_df.fillna(value=average_value, subset=['DiscountedPrice'])
@@ -68,6 +62,16 @@ filtered_df = filtered_df.withColumn(
 filtered_df = filtered_df.withColumn("Discount", col("Discount").cast("int"))
 
 filtered_df.select("Discount").show()
+pattern_with_k = r'(\d+)k'
+pattern_without_k = r'(\d+)'
+filtered_df.select("itemSoldCntShow").show()
+filtered_df = filtered_df.withColumn(
+    "itemSoldCntShow",
+    when(col("itemSoldCntShow").contains("k"), (regexp_extract(col("itemSoldCntShow"), pattern_with_k, 1).cast("int") * 1000))
+    .when(col("itemSoldCntShow").rlike(pattern_without_k), regexp_extract(col("itemSoldCntShow"), pattern_without_k, 1).cast("int"))
+    .otherwise(0)
+)
+filtered_df.select("itemSoldCntShow").show()
 
 text_columns = ['NAME', 'sellerName', 'brandName', 'location']
 pattern1 = r'\[.*?\]'
@@ -109,18 +113,22 @@ dim_brand_df = spark.createDataFrame(pd_dimbrand_df)
 dim_seller_df = spark.createDataFrame(pd_dimseller_df)
 
 joined_df = filtered_df.join(dim_category_df, filtered_df.categories == dim_category_df.categoryname, "inner")
+joined_df = joined_df.drop('categories', 'categoryname')  
+joined_df.show(truncate=False)
 
-# Show the joined DataFrame
-
-     
-joined_df1 = joined_df.join(dim_brand_df, joined_df.brandName == dim_brand_df.brandname, "inner")  
+joined_df1 = joined_df.join(dim_brand_df, joined_df.brandName == dim_brand_df.brandname, "inner")
+joined_df1 = joined_df1.drop('brandName', 'brandname', 'yearofex', 'brandcountry') 
 joined_df1.show(truncate=False)
+
 joined_df2 = joined_df1.join(dim_seller_df, joined_df1.sellerName == dim_seller_df.sellername, 'inner')
+joined_df2 = joined_df2.drop('sellerlocation', 'yearofex', 'sellername', 'location', 'sellerName')
 joined_df2.show(truncate=False)
+
 pandas_df = joined_df2.toPandas()
 
 
-pandas_df.to_sql('test1', engine, if_exists='append', index=False)
+
+pandas_df.to_sql('tes', engine, if_exists='append', index=False)
 
 
 engine.dispose()
